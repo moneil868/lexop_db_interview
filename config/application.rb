46dcaa55203fcbc86a48c0021c031a7e3126840c
Marlon O'Neil <hello@marlononeil.com>
2022-11-28 08:59:01 -0500

    first commit

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module LexopInterview
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.generators.system_tests = nil
+  end
+end
