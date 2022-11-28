3a44d146255c60d3d4347816365bd6a93103caa1
Marlon O'Neil <hello@marlononeil.com>
2022-11-28 10:14:52 -0500

    Configure Tailwind files as needed

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,27 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './vendor/javascript/tailwindcss-stimulus-components.js',
+    './app/components/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      minHeight: {
+        'table-min': '45rem',
+      },
+      colors: {
+        sky: colors.sky,
+        teal: colors.teal,
+        rose: colors.rose,
       },
     },
   },
